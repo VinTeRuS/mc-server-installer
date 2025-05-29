48e395c0265ce20ce33cc052c5ca6b1ce97c7f0a
WaterBucket <125190351+Roman12kYT@users.noreply.github.com>
2025-05-29 19:06:33 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -212,24 +212,24 @@ main_menu() {
         clear; display_header "${menu_title:-Minecraft Server Installer}"
         echo "${menu_select_server_type:-Выберите действие или тип сервера:}"
         echo "--- Установка ---"
-        echo "1) ${menu_option_paper:-Paper}"
-        echo "2) ${menu_option_vanilla:-Vanilla}"
-        echo "3) ${menu_option_fabric:-Fabric}"
-        echo "4) ${menu_option_forge:-Forge}"
-        echo "4.1) ${menu_option_neoforge:-NeoForge}
+        echo "1 ${menu_option_paper:-Paper}"
+        echo "2 ${menu_option_vanilla:-Vanilla}"
+        echo "3 ${menu_option_fabric:-Fabric}"
+        echo "4 ${menu_option_forge:-Forge}"
+        echo "4.1 ${menu_option_neoforge:-NeoForge}
         echo "5 ${menu_option_purpur:-Purpur}"
         echo "--- Управление ---"
-        echo "6) ${menu_option_manage_servers:-Управление серверами}"
-        echo "7) ${menu_option_settings:-Настройки}"
+        echo "6 ${menu_option_manage_servers:-Управление серверами}"
+        echo "7 ${menu_option_settings:-Настройки}"
         echo "--- Прочее ---"; echo "9) ${menu_option_exit:-Выход}"
         read_input "${prompt_choice:- > }" CHOICE
         case $CHOICE in
-            1) source "$FUNCTIONS_DIR/install_paper.sh"; install_paper_server ;;
-            2) source "$FUNCTIONS_DIR/install_vanilla.sh"; install_vanilla_server ;;
-            3) source "$FUNCTIONS_DIR/install_fabric.sh"; install_fabric_server ;;
-            4) source "$FUNCTIONS_DIR/install_forge.sh"; install_forge_server ;;
+            1 source "$FUNCTIONS_DIR/install_paper.sh"; install_paper_server ;;
+            2 source "$FUNCTIONS_DIR/install_vanilla.sh"; install_vanilla_server ;;
+            3 source "$FUNCTIONS_DIR/install_fabric.sh"; install_fabric_server ;;
+            4 source "$FUNCTIONS_DIR/install_forge.sh"; install_forge_server ;;
             5 source "$FUNCTIONS_DIR/install_purpur.sh"; install_purpur_server ;;
-            4.1) source "$FUNCTIONS_DIR/install_neoforge.sh"; install_neoforge_server ;;
+            4.1 source "$FUNCTIONS_DIR/install_neoforge.sh"; install_neoforge_server ;;
             6) manage_servers_menu ;;
             7) settings_menu ;;
             9) display_info "Выход из установщика."; exit 0 ;;
